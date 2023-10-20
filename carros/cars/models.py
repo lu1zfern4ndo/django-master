55b069bfe792a74eeaf304ad859aa0d02275eb47
Luiz Fernando <luiz-fernando@outlook.com>
2023-10-20 11:09:38 -0300

    Criado model Cars

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField()
+
+    def __str__(self) -> str:
+        return f'{self.model} = {self.brand}'
