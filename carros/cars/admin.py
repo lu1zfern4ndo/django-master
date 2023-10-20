fc2c091ab1693d14ba9ff1c1cdcb110eda3496af
Luiz Fernando <luiz-fernando@outlook.com>
2023-10-20 17:14:29 -0300

    Registrado Brand ao painel de admin do django

--- carros/cars/admin.py
+++ carros/cars/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model',)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
