eed026aa4d266e8df6136967d1c59830a082d8d9
Luiz Fernando <luiz-fernando@outlook.com>
2023-10-20 17:16:18 -0300

    Criado model Brand

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 
+class Brand(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Car(models.Model):
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField()
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self) -> str:
         return self.model
