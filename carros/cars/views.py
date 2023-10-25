25f8f81fb4ffd32fcd50eba390cdb9304d40ad45
Luiz Fernando <luiz-fernando@outlook.com>
2023-10-25 15:23:03 -0300

    Adicionado ordenação de carros no ORM

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -3,5 +3,8 @@ from . models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+    if search:
+        cars = cars.filter(model__icontains=search)
     return render(request, 'cars/cars.html', context={'cars': cars})
