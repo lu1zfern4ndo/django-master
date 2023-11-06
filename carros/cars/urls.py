36670faa55fe0c6d92b2ac461c53afbd0e04fe99
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-06 13:01:52 -0300

    Criada URL new_car

--- carros/cars/urls.py
+++ carros/cars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cars/', views.cars_view, name='cars_list')
+    path('cars/', views.cars_view, name='cars_list'),
+    path('new_car/', views.new_car, name='new_car'),
 ]
