33328a5d561c1f5aa6b9a9e3c307b707356873cd
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-06 13:04:16 -0300

    Separado CSS do HTML

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from . models import Car
+from django.shortcuts import render, redirect
+from .models import Car
+from .forms import CarForm
 
 
 def cars_view(request):
@@ -8,3 +9,14 @@ def cars_view(request):
     if search:
         cars = cars.filter(model__icontains=search)
     return render(request, 'cars/cars.html', context={'cars': cars})
+
+
+def new_car(request):
+    if request.method == 'POST':
+        new_car = CarForm(request.POST, request.FILES)
+        if new_car.is_valid():
+            new_car.save()
+            return redirect('cars_list')
+    elif request.method == 'GET':
+        new_car = CarForm()
+    return render(request, 'cars/new_car.html', context={ 'new_car': new_car })
