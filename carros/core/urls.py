fd9e62b6fe8dfcd514f2f9a253d504e90aeab2fe
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-10 14:24:33 -0300

    Adicionado urls para registro de usuários

--- carros/core/urls.py
+++ carros/core/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cars.urls'))
+    path('', include('cars.urls')),
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
