e40a4e0b0070c67b09f29b92b6b493690c993b9c
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-10 14:26:26 -0300

    Alterada class CarModelForm para herdar de modelForm

--- carros/cars/forms.py
+++ carros/cars/forms.py
@@ -1,29 +1,5 @@
-from typing import Any
 from django import forms
-from cars.models import Brand, Car
-
-
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField()
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
-    def save(self):
-        car = Car(
-            model=self.cleaned_data['model'],
-            brand=self.cleaned_data['brand'],
-            factory_year=self.cleaned_data['factory_year'],
-            model_year=self.cleaned_data['model_year'],
-            plate=self.cleaned_data['plate'],
-            value=self.cleaned_data['value'],
-            photo=self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+from cars.models import Car
 
 
 class CarModelForm(forms.ModelForm):
@@ -43,3 +19,26 @@ class CarModelForm(forms.ModelForm):
             self.add_error(
                 'factory_year', 'Não é possível cadastrar carros fabricados antes de 1975')
         return factory_year
+
+
+# class CarForm(forms.Form):
+#     model = forms.CharField(max_length=200)
+#     brand = forms.ModelChoiceField(Brand.objects.all())
+#     factory_year = forms.IntegerField()
+#     model_year = forms.IntegerField()
+#     plate = forms.CharField()
+#     value = forms.FloatField()
+#     photo = forms.ImageField()
+
+#     def save(self):
+#         car = Car(
+#             model=self.cleaned_data['model'],
+#             brand=self.cleaned_data['brand'],
+#             factory_year=self.cleaned_data['factory_year'],
+#             model_year=self.cleaned_data['model_year'],
+#             plate=self.cleaned_data['plate'],
+#             value=self.cleaned_data['value'],
+#             photo=self.cleaned_data['photo'],
+#         )
+#         car.save()
+#         return car
